c4b81772ec7860bc818e2bd1812a3caa307ebd6a
KHP53 <khp4121@gmail.com>
2024-04-06 00:03:09 -0230

    fixed could not get /map page upon reload

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use('/api', routes);
 
-app.get('/', async (req, res) => {
+app.get('/*', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'view/build', 'index.html'));
 });
 
